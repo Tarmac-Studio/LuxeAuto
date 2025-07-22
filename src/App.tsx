daa421b5744a5cce2c2b11a3e94f7120c048cb8f
Dev-Titan001 <taiwoadebayo541@gmail.com>
2025-07-22 21:58:02 +0100

    LuxeAuto setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to LuxeAuto</h1>
+      <p className="mt-4 text-white-700 ">
+        Welcome to LuxeAutos, where we invite passionate individuals and dealers
+        to join our exclusive network of luxury car sellers. Elevate your
+        selling experience by showcasing your exceptional inventory to a
+        discerning global audience.
+      </p>
+    </div>
+  );
+}
+
+export default App;
